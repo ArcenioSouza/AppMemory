40b906a906dda10e495cafcb43e3ba52bae457c2
ArcenioSouza <arcenio_neto@icloud.com>
2021-10-04 10:18:19 -0300

    Adiciona css ao form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,57 +1,29 @@
 import React from 'react'
-import styled from 'styled-components'
+import * as S from './form.module.css'
 
 const Form = ({onChange}) => {
 
-   const Container = styled.div `
-      width: 90%;
-      border: 2px double #000000;
-      border-radius: 25px;
-      padding: 15px 0 15px 0;
-      margin: 0 5% 0 5%;      
-   `
-
-   const Wrapper = styled.div `
-      display: flex;
-      justify-content: center;
-      text-align: right;
-   `
-
-   const WrapperForm = styled.form `
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      text-align: right;
-   `
-
-   const Input = styled.input `
-      color: #000;
-      padding: 5px;
-      margin-left: 2px;
-      cursor: text;
-      background-color: #fafafa;
-      border: 1px solid #000;
-      border-radius: 5px;
-   `
-
-   const Label = styled.label `
-      font-size: 1.2rem;
-      color: #000;
-      margin: 2px;
-   `
-   return (
-      <Container>
-         <Wrapper>
-            <WrapperForm>
-               <Label htmlFor="title">Title:
-                  <Input type="text" name="title" id="title" onChange={onChange} />
-               </Label>
-               <Label htmlFor="description">Description:
-                  <Input type="text" name="description" id="description" onChange={onChange} />
-               </Label>
-            </WrapperForm>
-         </Wrapper>
-      </Container>
+   return (   
+      <div className={S.container}>
+         <div className={S.form}>
+            <div className={S.row}>
+               <div className={S.columnLabel}>
+                  <label htmlFor="title">Title: </label>
+               </div>
+               <div className={S.columnInput}>
+                  <input type="text" name="title" id="title" onChange={onChange} />
+               </div>
+            </div>
+            <div className={S.row}>
+               <div className={S.columnLabel}>
+                  <label htmlFor="description">Description: </label>  
+               </div>
+               <div className={S.columnInput}>
+                  <input type="text" name="description" id="description" onChange={onChange} />
+               </div>
+            </div>                
+         </div>
+      </div>        
    )
 }
 
