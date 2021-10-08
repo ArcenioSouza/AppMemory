1134d6a35aaf217de451542a746b39f13b6e304a
ArcenioSouza <arcenio_neto@icloud.com>
2021-10-08 15:55:29 -0300

    Adiciona coreçao do styled no component Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,30 +3,30 @@ import styled from "styled-components";
 import { api } from "../../services/api";
 import Button from "../button/Button";
 
+const Container = styled.div`
+width: 300px;
+display: flex;
+border: 2px solid var(--third-color);
+border-radius: 25px;
+align-items: center;
+padding: 15px;
+margin: 5px;
+background-color: #fff;
+color: var(--third-color);
+`;
+
+const InfoCards = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+text-align: center;
+padding: 15px;
+`;
+
 const Card = ({ id, title, description }) => {
   const handleRemove = () => {
     api.delete(`/cards/${id}`);
   };
-
-  const Container = styled.div`
-    width: 300px;
-    display: flex;
-    border: 2px solid var(--third-color);
-    border-radius: 25px;
-    align-items: center;
-    padding: 15px;
-    margin: 5px;
-    background-color: #fff;
-    color: var(--third-color);
-  `;
-
-  const InfoCards = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    padding: 15px;
-  `;
 
   return (
     <Container>
