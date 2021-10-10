c1ab12a2d56288c9223812dc30a157ccae25b1fb
ArcenioSouza <arcenio_neto@icloud.com>
2021-10-10 10:48:59 -0300

    Adiciona separaçao de styled ao component card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,50 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { api } from "../../services/api";
 import { useState } from "react";
-
-
-const Container = styled.div`
-    width: 300px;
-    display: flex;
-    border: 2px solid var(--third-color);
-    border-radius: 25px;
-    align-items: center;
-    padding: 15px;
-    margin: 5px;
-    background-color: #fff;
-    color: var(--third-color);
-  `;
-
-  const InfoCards = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    padding: 15px;
-  `;
-
-  const WrapperButtons = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `
-
-  const Sbutton = styled.button`
-    background-color: var(--second-color);
-    color: #fff;
-    padding: 5px 10px 5px 10px;
-    margin-right: 10px;
-    border: 2px solid var(--third-color);
-    border-radius: 10px;
-    font-size: 1rem;
-    font-weight: bold;
-    transition: 0.6s ease-in-out;
-    :hover{
-      cursor: pointer;
-      background-color: #fff;
-      color: var(--third-color);
-    }
-  `
+import * as S from "./styledCard"
 
 const Card = ({ id, title, description, updateApi, setUpdateApi, onClick }) => {
 
@@ -75,8 +32,8 @@ const Card = ({ id, title, description, updateApi, setUpdateApi, onClick }) => {
   
 
   return (
-    <Container>
-      <InfoCards>
+    <S.Container>
+      <S.InfoCards>
         {
           isEdit ? 
           (
@@ -94,12 +51,12 @@ const Card = ({ id, title, description, updateApi, setUpdateApi, onClick }) => {
           )
         }
         
-        <WrapperButtons>
-          <Sbutton onClick={() => onClick(id)}>Remover</Sbutton>  
-          {isEdit ? <Sbutton onClick={handleSalvar}>Salvar</Sbutton> : <Sbutton onClick={() => setIsEdit(true)}>Editar</Sbutton> }
-        </WrapperButtons>                 
-      </InfoCards>
-    </Container>
+        <S.WrapperButtons>
+          <S.Button onClick={() => onClick(id)}>Remover</S.Button>  
+          {isEdit ? <S.Button onClick={handleSalvar}>Salvar</S.Button> : <S.Button onClick={() => setIsEdit(true)}>Editar</S.Button> }
+        </S.WrapperButtons>                 
+      </S.InfoCards>
+    </S.Container>
   );
 };
 
---
+++ src/components/card/styledCard.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 300px;
+    display: flex;
+    border: 2px solid var(--third-color);
+    border-radius: 25px;
+    align-items: center;
+    padding: 15px;
+    margin: 5px;
+    background-color: #fff;
+    color: var(--third-color);
+  `;
+
+export const InfoCards = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    padding: 15px;
+  `;
+
+export const WrapperButtons = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
+
+export const Button = styled.button`
+    background-color: var(--second-color);
+    color: #fff;
+    padding: 5px 10px 5px 10px;
+    margin-right: 10px;
+    border: 2px solid var(--third-color);
+    border-radius: 10px;
+    font-size: 1rem;
+    font-weight: bold;
+    transition: 0.6s ease-in-out;
+    :hover{
+      cursor: pointer;
+      background-color: #fff;
+      color: var(--third-color);
+    }
+  `
