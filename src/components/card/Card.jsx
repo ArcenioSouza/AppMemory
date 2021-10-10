33675541964b4fef43dd8e6c0b6120758bf7b207
ArcenioSouza <arcenio_neto@icloud.com>
2021-10-10 10:56:26 -0300

    Adiciona separacao de styled ao component Note

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,59 +2,61 @@ import React from "react";
 import styled from "styled-components";
 import { api } from "../../services/api";
 import { useState } from "react";
-import * as S from "./styledCard"
+import * as S from "./styledCard";
 
 const Card = ({ id, title, description, updateApi, setUpdateApi, onClick }) => {
-
-  const [isEdit, setIsEdit] = useState(false)
+  const [isEdit, setIsEdit] = useState(false);
   const [input, setInput] = useState({
     title: title,
-    description: description
-  })
+    description: description,
+  });
 
-  const handleOnChange = ({target}) => {
-    const name = target.name
-    const value = target.value
+  const handleOnChange = ({ target }) => {
+    const name = target.name;
+    const value = target.value;
     setInput({
       ...input,
-      [name]: value
-      //name representa o target que está sendo feito a mudança, no caso se for no title, o name será = title e assim vai...
-    })
-  }
-
- 
+      [name]: value,
+    });
+  };
 
   const handleSalvar = () => {
-    api.put(`/cards/${id}`, input).then(() => setUpdateApi(!updateApi))
-    setIsEdit(false)
-  }
-
-  
+    api.put(`/cards/${id}`, input).then(() => setUpdateApi(!updateApi));
+    setIsEdit(false);
+  };
 
   return (
     <S.Container>
       <S.InfoCards>
-        {
-          isEdit ? 
-          (
-            <>
-              <input type="text" name="title" value={input.title} onChange={handleOnChange}/>
-              <textarea  name="description" value={input.description} onChange={handleOnChange}/>
-            </>
-          ) 
-          : 
-          (
-            <>
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </>
-          )
-        }
-        
+        {isEdit ? (
+          <>
+            <input
+              type="text"
+              name="title"
+              value={input.title}
+              onChange={handleOnChange}
+            />
+            <textarea
+              name="description"
+              value={input.description}
+              onChange={handleOnChange}
+            />
+          </>
+        ) : (
+          <>
+            <h2>{title}</h2>
+            <p>{description}</p>
+          </>
+        )}
+
         <S.WrapperButtons>
-          <S.Button onClick={() => onClick(id)}>Remover</S.Button>  
-          {isEdit ? <S.Button onClick={handleSalvar}>Salvar</S.Button> : <S.Button onClick={() => setIsEdit(true)}>Editar</S.Button> }
-        </S.WrapperButtons>                 
+          <S.Button onClick={() => onClick(id)}>Remover</S.Button>
+          {isEdit ? (
+            <S.Button onClick={handleSalvar}>Salvar</S.Button>
+          ) : (
+            <S.Button onClick={() => setIsEdit(true)}>Editar</S.Button>
+          )}
+        </S.WrapperButtons>
       </S.InfoCards>
     </S.Container>
   );
--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -1,53 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import * as S from "./styledNote"
 import { api } from "../../services/api";
 import { useState } from "react";
 
-const Container = styled.div`
-  width: 300px;
-  display: flex;
-  border: 2px solid var(--third-color);
-  border-radius: 25px;
-  align-items: center;
-  padding: 15px;
-  margin: 5px;
-  background-color: #fff;
-  color: var(--third-color);
-`;
-
-const InfoCards = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  padding: 15px;
-`;
-
-const Sbutton = styled.button`
-  background-color: var(--second-color);
-  color: #fff;
-  padding: 5px 10px 5px 10px;
-  margin-right: 10px;
-  border: 2px solid var(--third-color);
-  border-radius: 10px;
-  font-size: 1rem;
-  font-weight: bold;
-  transition: 0.6s ease-in-out;
-  :hover {
-    cursor: pointer;
-    background-color: #fff;
-    color: var(--third-color);
-  }
-`;
-
-const WrapperButtons = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `
-
 const Note = ({ id, title, description, updateApi, setUpdateApi }) => {
-
   const [editForm, setEditForm] = useState({
     title: title,
     description: description,
@@ -56,38 +12,38 @@ const Note = ({ id, title, description, updateApi, setUpdateApi }) => {
   const [isEdit, setIsEdit] = useState(false);
 
   const handleRemove = () => {
-    api.delete(`/notes/${id}`).then(() => setUpdateApi(!updateApi))
+    api.delete(`/notes/${id}`).then(() => setUpdateApi(!updateApi));
   };
 
-  const handleOnChange = ({target}) => {
-    const name = target.name //salva na variável a string contida em name do input.
-    const value = target.value //salva na variável o valor contido no input.
+  const handleOnChange = ({ target }) => {
+    const name = target.name; //salva na variável a string contida em name do input.
+    const value = target.value; //salva na variável o valor contido no input.
     setEditForm({
       ...editForm, //retorna o valor que está no card ao input.
-      [name]: value //atribui a mudança que foi feita no valor do input a variável name que carrega o nome title ou description.
-    })
-  }
+      [name]: value, //atribui a mudança que foi feita no valor do input a variável name que carrega o nome title ou description.
+    });
+  };
 
   const handleSalvar = () => {
-    api.put(`/notes/${id}`, editForm).then(() => setUpdateApi(!updateApi))
-    setIsEdit(false)
-  }
+    api.put(`/notes/${id}`, editForm).then(() => setUpdateApi(!updateApi));
+    setIsEdit(false);
+  };
 
   return (
-    <Container>
-      <InfoCards>
+    <S.Container>
+      <S.InfoCards>
         {isEdit ? (
           <>
             <input
               name="title"
-              type="text" 
-              value={editForm.title} 
-              onChange={handleOnChange} 
+              type="text"
+              value={editForm.title}
+              onChange={handleOnChange}
             />
-            <textarea 
+            <textarea
               name="description"
               value={editForm.description}
-              onChange={handleOnChange} 
+              onChange={handleOnChange}
             />
           </>
         ) : (
@@ -97,16 +53,16 @@ const Note = ({ id, title, description, updateApi, setUpdateApi }) => {
           </>
         )}
 
-        <WrapperButtons>
-          <Sbutton onClick={handleRemove}>Remover</Sbutton>
-          {isEdit ? 
-            <Sbutton onClick={handleSalvar}>Salvar</Sbutton> 
-          : 
-            <Sbutton onClick={() => setIsEdit(true)}>Editar</Sbutton>
-          }
-        </WrapperButtons>
-      </InfoCards>
-    </Container>
+        <S.WrapperButtons>
+          <S.Button onClick={handleRemove}>Remover</S.Button>
+          {isEdit ? (
+            <S.Button onClick={handleSalvar}>Salvar</S.Button>
+          ) : (
+            <S.Button onClick={() => setIsEdit(true)}>Editar</S.Button>
+          )}
+        </S.WrapperButtons>
+      </S.InfoCards>
+    </S.Container>
   );
 };
 
