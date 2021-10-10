15ec504aa6b44c78eb4f3fc9bf8af47b01a9a587
ArcenioSouza <arcenio_neto@icloud.com>
2021-10-10 12:05:31 -0300

    Iniciando modernizacao do appmemory

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import Header from "./components/header/header";
 import Notes from "./pages/notes/Notes";
 import Reminder from "./pages/reminder/Reminder";
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import styled from "styled-components";
+
+const Container = styled.div `
+background-color: aquamarine;
+  width: 100vmax;
+`
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+    <Container>
+      {/* <BrowserRouter>
         <Header />
         <Switch>
           <Route path='/' exact component={Reminder}/>
           <Route path='/notes' exact component={Notes}/>
         </Switch>      
-      </BrowserRouter>
-    </div>
+      </BrowserRouter> */}
+    </Container>
   );
 }
 
